f854a412ba7590af08e19d2fc7ee318d96064c07
jekajolob <onyxlsd24@gmail.com>
2017-06-21 13:09:59 +0300

    local standalone server

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,7 +1,7 @@
 exports.config = {
 
-    user: process.env.SAUCE_USERNAME,
-    key: process.env.SAUCE_ACCESS_KEY,
+    //user: process.env.SAUCE_USERNAME,
+    //key: process.env.SAUCE_ACCESS_KEY,
     //
     // ==================
     // Specify Test Files
